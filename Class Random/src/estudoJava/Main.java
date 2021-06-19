a07856730ae454b4641720ffb77cc5f17c73384b
Jose carlos <josecarlos2503@hotmail.com>
2021-06-19 10:25:24 -0300

    Gerador de numeros para Quina

--- Class Random/src/estudoJava/Main.java
+++ Class Random/src/estudoJava/Main.java
@@ -1,21 +1,13 @@
-package estudoJava;
-
-import java.util.Random;
-
-;
-
 public class Main {
 
 	public static void main(String[] args) {
-		// instancia um objeto da classe randon usando o construtor padrão
 		Random gerador = new Random();
-		
-		// imprime um numero inteiro aleatorio de 0 a 100
-		System.out.println(gerador.nextInt(100) + 1);
-		
-		// imprime um numero inteiro aleatorio de 1 a 10
-		// System.out.println(gerador.nextInt(10) + 1);
-		
+		// Autor: José Carlos de oliveira
+		System.out.println(gerador.nextInt(80)+1);
+		System.out.println(gerador.nextInt(80)+1);
+		System.out.println(gerador.nextInt(80)+1);
+		System.out.println(gerador.nextInt(80)+1);
+		System.out.println(gerador.nextInt(80)+1);
 		
 
 	}
